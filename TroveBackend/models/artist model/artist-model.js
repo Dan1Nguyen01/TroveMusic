c7c8075bd53329f558cee865d8f9dadfd7294e3f
goosefring <bhattirehan.rb@gmail.com>
2023-04-12 10:10:45 -0600

    added profile picture

--- TroveBackend/models/artist model/artist-model.js
+++ TroveBackend/models/artist model/artist-model.js
@@ -14,6 +14,7 @@ const artistSchema = mongoose.Schema({
 
   artistImg: {
     type: String,
+    default: "https://firebasestorage.googleapis.com/v0/b/helical-analyst-376421.appspot.com/o/images%2FartistPicPlaceholder.png?alt=media&token=2e12a07f-4c39-4029-8c7c-d30fcbcfa84c"
   },
 
   email: {
